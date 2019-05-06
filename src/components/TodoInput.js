b3448ca049e09ffe361249326c84fa03429373e1
Nelson Anane <nelsonanane@Nelsons-iMac.fios-router.home>
2019-05-06 10:57:05 -0400

    comment

---
+++ src/components/TodoInput.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class TodoInput extends Component {
+    render() {
+        return (
+            <h1>hello from todo input</h1>
+        )
+    }
+}
