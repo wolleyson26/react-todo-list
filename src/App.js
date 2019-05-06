ad4821c1bbe1e89a530287689266e6bd88fe0235
Nelson Anane <nelsonanane@Nelsons-iMac.fios-router.home>
2019-05-06 11:13:41 -0400

    app component structure

--- src/App.js
+++ src/App.js
@@ -5,11 +5,33 @@ import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
 export default class App extends Component {
+
+  state = {
+    items: [{ id: 1, title: 'wake up' }, { id: 2, title: 'make breakfast' }],
+    id: uuid(),
+    item: '',
+    editItem: false
+  };
+
+  handleChange = (e) => { console.log('handle change') }
+  handleSubmit = (e) => { console.log('handle submit') }
+  clearList = () => { console.log('clear list') }
+  handleDelete = (id) => { console.log(`handle delete ${id}`) }
+  handleEdit = (id) => { console.log(`edit ${id}`) }
+
   render() {
+
+
+
     return (
-      <div className='app'>
-        <TodoInput />
-        <TodoList />
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 className='text-capitalize text-center'>todo input</h3>
+            <TodoInput item={this.state.item} handleChange={this.handleChange} handleSubmit={this.handleSubmit} editItem={this.editItem} />
+            <TodoList items={this.state.items} clearList={this.clearList} handleDelete={this.handleDelete} handleEdit={this.handleEdit} />
+          </div>
+        </div>
       </div>
     )
   }
