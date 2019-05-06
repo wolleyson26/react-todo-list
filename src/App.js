77e6179a98bbeafcacc8f73693e8587118056fbc
Nelson Anane <nelsonanane@Nelsons-iMac.fios-router.home>
2019-05-06 11:24:53 -0400

    todoinput

--- src/App.js
+++ src/App.js
@@ -28,8 +28,18 @@ export default class App extends Component {
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-4">
             <h3 className='text-capitalize text-center'>todo input</h3>
-            <TodoInput item={this.state.item} handleChange={this.handleChange} handleSubmit={this.handleSubmit} editItem={this.editItem} />
-            <TodoList items={this.state.items} clearList={this.clearList} handleDelete={this.handleDelete} handleEdit={this.handleEdit} />
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.editItem}
+            />
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            />
           </div>
         </div>
       </div>
